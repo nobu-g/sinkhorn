abd0778aa591e6dd928ca0b218a219bd18db6d8c
nobu-g <ueda-n127@hotmail.co.jp>
2020-08-29 23:49:45 +0900

    enable pick and move points

--- sinkhorn.py
+++ sinkhorn.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 from scipy.spatial.distance import cdist
 
 # Calculate P & W using POT (https://pythonot.github.io/)
@@ -10,9 +11,7 @@ import ot
 
 def calc_(a, b, cost: np.ndarray, gamma: float):
     P = ot.sinkhorn(a, b, cost, gamma)
-    W = ot.sinkhorn2(a, b, cost, gamma)
-
-    return P, W
+    return P
 
 
 def calc(a, b, cost: np.ndarray, gamma: float):
@@ -27,8 +26,12 @@ def calc(a, b, cost: np.ndarray, gamma: float):
         v = b / np.matmul(K.T, u)  # (m,), b / (K^T x u)
 
     P = np.matmul(np.matmul(np.diag(u), K), np.diag(v))  # (n, m), diag(u) x K x diag(v)
-    W = np.sum(P * cost)
-    return P, W
+    return P
+
+
+artist = None
+idx1 = None
+idx2 = None
 
 
 def show(x, y, P, W):
@@ -37,10 +40,12 @@ def show(x, y, P, W):
     fig: Figure = plt.figure()
     ax: Axes = fig.add_subplot(1, 1, 1)
     ax.set_title('OT matrix with samples')
-    # plot2D_samples_mat(x, y, P, c=[.5, .5, 1.])
-    ax.plot(x[:, 0], x[:, 1], '+r', label='Source samples')
-    ax.plot(y[:, 0], y[:, 1], 'xb', label='Target samples')
+    line1, = ax.plot(x[:, 0], x[:, 1], 'o', label='Source samples', picker=True)
+    line2, = ax.plot(y[:, 0], y[:, 1], 'o', label='Target samples', picker=True)
     ax.legend(loc=0)
+
+    ax.xaxis.set_pickradius(10)
+    ax.yaxis.set_pickradius(10)
 
     mx = P.max()
     thr = 1e-8
@@ -49,6 +54,56 @@ def show(x, y, P, W):
             alpha = P[i, j] / mx
             if alpha > thr:
                 ax.plot([x[i, 0], y[j, 0]], [x[i, 1], y[j, 1]], alpha=alpha, c=[0.5, 0.5, 1])
+
+    def motion(event):
+        global artist, idx1, idx2
+        if artist is None:
+            return
+        assert isinstance(artist, Line2D)
+
+        if event.xdata is None or event.ydata is None:
+            artist = None
+            return
+
+        idx = idx1 if idx1 is not None else idx2
+
+        xdata = artist.get_xdata()
+        ydata = artist.get_ydata()
+        print(xdata.shape)
+        xdata[idx] = event.xdata
+        ydata[idx] = event.ydata
+        print(f'({event.xdata:.2}, {event.ydata:.2})')
+        print(f'sum: {np.sum(xdata)}')
+        # artist.set_data(xdata, ydata)
+        fig.canvas.draw()
+
+    def onpick(event):
+        print('picked')
+        global artist, idx1, idx2
+        if event.artist is line1:
+            artist = event.artist
+            idx1 = event.ind
+            print(idx1)
+        if event.artist is line2:
+            artist = event.artist
+            idx2 = event.ind
+            print(idx2)
+        # xdata = artist.get_xdata()
+        # ydata = artist.get_ydata()
+        # print(xdata.shape)
+        # ind = event.ind
+        # print(f'onpick points: ({xdata[ind]}, {ydata[ind]})')
+        # fig.canvas.draw()
+
+    def release(event):
+        global artist, idx1, idx2
+        artist = None
+        idx1 = None
+        idx2 = None
+
+    fig.canvas.mpl_connect('motion_notify_event', motion)
+    fig.canvas.mpl_connect('pick_event', onpick)
+    fig.canvas.mpl_connect('button_release_event', release)
 
     plt.show()
 
@@ -70,16 +125,11 @@ def main():
     y = np.stack([np.random.multivariate_normal(mu_y, cov_y) for _ in range(m)])  # (m, d)
     b = np.ones(m) / m  # (m,). same mass at each position
 
-    # plt.figure()
-    # plt.plot(x[:, 0], x[:, 1], '+r', label='Source samples')  # '+' colored by red
-    # plt.plot(y[:, 0], y[:, 1], 'xb', label='Target samples')  # 'x' colored by blue
-    # plt.legend(loc=0)
-    # plt.title('Source and target distributions.')
-
     # Transportation costs
     dist: np.ndarray = cdist(x, y, metric='euclidean')  # (n, m)
 
-    P, W = calc(a, b, cost=dist, gamma=gamma)
+    P = calc(a, b, cost=dist, gamma=gamma)
+    W = np.sum(P * dist)
 
     show(x, y, P, W)
 
